00e88812f355e7999365e4fb6c3ac37ce76625e8
Wesley Suebring <WesleyS@CURSISTZ22>
2016-09-12 15:44:23 +0200

    TDD Dag05

---
+++ Minor.dag5.TDD/minor.dag5.TDD.Math/Math.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+public class Math
+{
+    public Math()
+    {
+    }
+
+    public int Fact(int n)
+    {   
+        if (n <= 0)
+        {
+            throw new InvalidOperationException();
+        }
+
+        if (n == 1)
+        {
+            return 1;
+        }
+        return n * Fact(n - 1);
+    }
+}
---
+++ Minor.dag5.TDD/minor.dag5.TDD.Test/MathTest.cs
@@ -0,0 +1,58 @@
+﻿using Xunit;
+
+namespace minor.dag5.TDD.Test
+{
+    public class MathTest
+    {
+        [Fact]
+        public void Fact0IsExpectInvalidOperation()
+        {
+            //arange
+            var target = new Math();
+
+            //act
+            System.Action action = () => target.Fact(0);
+
+            //assert
+            Assert.Throws<System.InvalidOperationException>(action);
+        }
+        [Fact]
+        public void Fact1Is1()
+        {
+            //arange
+            var target = new Math();
+
+            //act
+            int result = target.Fact(1);
+
+            //assert
+            Assert.Equal(1, result);
+        }
+        [Fact]
+        public void Fact2Is2()
+        {
+            //arange
+            var target = new Math();
+
+            //act
+            int result = target.Fact(2);
+
+            //assert
+            Assert.Equal(2, result);
+        }
+
+        [Fact]
+        public void Fact3Is6()
+        {
+            //arange
+            var target = new Math();
+
+            //act
+            int result = target.Fact(3);
+
+            //assert
+            Assert.Equal(6, result);
+        }
+
+    }
+}
