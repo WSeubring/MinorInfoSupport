af869a480c923e5e4cdc7ba375cf696acecb10ca
Wesley Suebring <WesleyS@CURSISTZ22>
2016-09-15 15:51:15 +0200

    Regex

--- Minor.Dag8.Regex/src/RegexBedragen/GetalChecker.cs
+++ Minor.Dag8.Regex/src/RegexBedragen/GetalChecker.cs
@@ -3,7 +3,7 @@ using System.Text.RegularExpressions;
 
 public class GetalChecker
 {
-    private static readonly Regex pattern = new Regex(@"^-?[0-9]{1,3}(,[0-9]{3})*([.][0-9]{2}$)");
+    private static readonly Regex pattern = new Regex(@"^-?[0-9]{1,3}(,[0-9]{3})*([.][0-9]{2})$");
 
     public bool Check(string getal)
     {
