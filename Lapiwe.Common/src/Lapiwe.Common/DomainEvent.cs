b8a47d61bfe3dfbfcc35d4231e3804bab144a60c
Wesley Seubring <WesleyS@CURSISTZ10>
2016-11-24 09:46:03 +0100

    Created common solution/project

---
+++ Lapiwe.Common/src/Lapiwe.Common/DomainEvent.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Lapiwe.Common
+{ 
+    public abstract class DomainEvent
+    {
+        public DateTime TimpeStamp { get; set; }
+        public string RoutingKey { get; set; }
+        public Guid CorrelationID { get; set; }
+
+    }
+}
