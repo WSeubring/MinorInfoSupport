66565fc7ab2d25f7f6d6b1f5c7a9d7384c8cbd8a
Wesley Seubring <WesleyS@CURSISTZ10>
2016-11-24 11:20:10 +0100

    KlantDetails init commit

--- Lapiwe.GMS.FrontEnd/src/Lapiwe.GMS.FrontEnd/Startup.cs
+++ Lapiwe.GMS.FrontEnd/src/Lapiwe.GMS.FrontEnd/Startup.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using Lapiwe.GMS.FrontEnd.ViewModels;
 
 namespace Lapiwe.GMS.FrontEnd
 {
@@ -37,6 +38,9 @@ namespace Lapiwe.GMS.FrontEnd
             services.AddApplicationInsightsTelemetry(Configuration);
 
             services.AddMvc();
+
+            services.AddDbContext<KlantGegegevensViewModel>(options =>
+                    options.UseSqlServer(Configuration["Data:MonumentenContext:ConnectionString"]));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
