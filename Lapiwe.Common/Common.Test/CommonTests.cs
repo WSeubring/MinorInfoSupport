46ed160340fdaddad34189574acd9b2af84e46dc
Wesley Seubring <WesleyS@CURSISTZ10>
2016-11-24 11:28:23 +0100

    Feedback pull request

---
+++ Lapiwe.Common/Common.Test/CommonTests.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Common.Test
+{
+    [TestClass]
+    public class CommonTests
+    {
+        [TestMethod]
+        public void TestRoutingKey()
+        {
+            // Arrange
+            TestEvent target = new TestEvent();
+
+            // Assert
+            Assert.AreEqual("Common.Test.TestEvent" ,target.RoutingKey);
+        }
+    }
+}
--- Lapiwe.Common/src/Lapiwe.Common/DomainEvent.cs
+++ Lapiwe.Common/src/Lapiwe.Common/DomainEvent.cs
@@ -4,9 +4,15 @@ namespace Lapiwe.Common
 { 
     public abstract class DomainEvent
     {
-        public DateTime TimpeStamp { get; set; }
-        public string RoutingKey { get; set; }
+        public DateTime TimeStamp { get; set; }
         public Guid CorrelationID { get; set; }
+        public string RoutingKey { get; set; }
 
+        public DomainEvent()
+        {
+            TimeStamp = DateTime.Now;
+            CorrelationID = new Guid();
+            RoutingKey = GetType().FullName;
+        }
     }
 }
