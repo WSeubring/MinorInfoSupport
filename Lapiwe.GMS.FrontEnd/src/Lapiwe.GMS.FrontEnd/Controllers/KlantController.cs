8f7ff7c806838b1a5450f725b96d2167f9cff5c8
Lars Celie <LarsC@CURSISTZ11>
2016-11-24 13:15:34 +0100

    inschrijven formulier

---
+++ Lapiwe.GMS.FrontEnd/src/Lapiwe.GMS.FrontEnd/Controllers/KlantController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Lapiwe.GMS.FrontEnd.Controllers
+{
+    public class KlantController : Controller
+    {
+        public IActionResult Inschrijven()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Registreer([FromBody] object ViewModelKlant)
+        {
+
+            return RedirectToAction("Index");
+        }
+    }
+}
