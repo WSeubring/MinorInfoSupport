c5a2296bdf1a230b2d40d912595fd6cd10084db8
Pim van Gurp <PimG@CURSISTZ12>
2016-11-24 09:16:06 +0100

    Klantbeheer service opzetten

---
+++ Lapiwe.KlantBeheerService/src/Lapiwe.KlantBeheerService.Export/Properties/AssemblyInfo.cs
@@ -0,0 +1,19 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Lapiwe.KlantBeheerService.Export")]
+[assembly: AssemblyTrademark("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("40fa9076-5c14-47f1-9f0f-0c0e32d24aca")]
---
+++ Lapiwe.KlantBeheerService/src/Lapiwe.KlantBeheerService.Facade/Program.cs
@@ -0,0 +1,13 @@
+﻿using Minor.WSA.Common.Contracts;
+using Minor.WSA.WSAEventbus;
+using System;
+
+namespace Lapiwe.KlantBeheerService.Facade
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+        }
+    }
+}
