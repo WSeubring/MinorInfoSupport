d4b62669b5a530a9b2111c77dabcf6311a7782c2
Wesley Seubring <WesleyS@CURSISTZ10>
2016-11-24 09:46:03 +0100

    Created common solution/project

---
+++ Lapiwe.Common/src/Lapiwe.Common/FuctionalError.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lapiwe.Common
+{
+    public class FunctionalError
+    {
+        public string ErrorCode { get; set; }
+        public string ErrorMessage { get; set; }
+
+        public FunctionalError(string errorCode, string errorMessage)
+        {
+            ErrorCode = errorCode;
+            ErrorMessage = errorMessage;
+        }
+    }
+}
